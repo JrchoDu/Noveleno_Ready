0d949494c7a9f8a7f803533a19b45dafe1e44b62
romelmaghinang <romel.maghinang@clairemontferrond.com>
2024-10-06 23:37:10 +0800

    test

--- controllers/pendingController.js
+++ controllers/pendingController.js
@@ -1,6 +1,37 @@
 const { Sequelize, Op } = require('sequelize');
 const User = require('../models/User'); 
 
+exports.getUserCountWithNullStatus = async (req, res) => {
+  try {
+    const count = await User.count({
+      where: {
+        status: null, // Only users with null status
+      },
+    });
+    res.json({ userCountWithNullStatus: count });
+  } catch (err) {
+    console.error('Error in getUserCountWithNullStatus:', err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+// Count users created within the last 14 days
+exports.getUserCountLast14Days = async (req, res) => {
+  try {
+    const count = await User.count({
+      where: {
+        createdAt: {
+          [Op.gte]: new Date(new Date() - 14 * 24 * 60 * 60 * 1000), // Created within the last 14 days
+        },
+      },
+    });
+    res.json({ userCountLast14Days: count });
+  } catch (err) {
+    console.error('Error in getUserCountLast14Days:', err.message);
+    res.status(500).send('Server error');
+  }
+};
+
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.findAll({
--- controllers/userController.js
+++ controllers/userController.js
@@ -19,37 +19,6 @@ exports.getUserCount = async (req, res) => {
   }
 };
 
-exports.getUserCountWithNullStatus = async (req, res) => {
-  try {
-    const count = await User.count({
-      where: {
-        status: null, // Only users with null status
-      },
-    });
-    res.json({ userCountWithNullStatus: count });
-  } catch (err) {
-    console.error('Error in getUserCountWithNullStatus:', err.message);
-    res.status(500).send('Server error');
-  }
-};
-
-// Count users created within the last 14 days
-exports.getUserCountLast14Days = async (req, res) => {
-  try {
-    const count = await User.count({
-      where: {
-        createdAt: {
-          [Op.gte]: new Date(new Date() - 14 * 24 * 60 * 60 * 1000), // Created within the last 14 days
-        },
-      },
-    });
-    res.json({ userCountLast14Days: count });
-  } catch (err) {
-    console.error('Error in getUserCountLast14Days:', err.message);
-    res.status(500).send('Server error');
-  }
-};
-
 exports.acceptUser = async (req, res) => {
   const { id } = req.params;
 
